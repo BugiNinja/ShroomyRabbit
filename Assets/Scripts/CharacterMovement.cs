4b28715825e213374d1d68c278d34b1b26164967
YouMyller <juumyllyla@gmail.com>
2017-09-22 22:57:48 +0300

    - player controls - enemy movement - 1st level prototype

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+	private Rigidbody2D myRigidBody;
+
+	public float movSpeed;
+
+	public float jumpPower;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myRigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+        //Input & call movement void
+
+        float horizontal = Input.GetAxis ("Horizontal");
+
+		movement (horizontal);
+	}
+
+	void OnCollisionStay2D(Collision2D coll)
+	{
+        //Ground check & jump
+        if (coll.gameObject.tag == "Ground" && Input.GetKey ("space")) 
+		{
+			myRigidBody.AddForce(Vector2.up * jumpPower, ForceMode2D.Force);
+		}
+	}
+
+	private void movement(float horizontal)
+	{
+        //Movement
+        myRigidBody.velocity = new Vector2(horizontal * movSpeed, myRigidBody.velocity.y); //-Vector2.right
+	} 
+}
---
+++ Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour {
+
+    private float enemySpeed;
+
+    public float enemyHealth;
+
+    private Rigidbody2D enemyBody;
+
+    private Transform myTrans;
+
+    public bool left;
+   
+	// Use this for initialization
+	void Start ()
+    {
+        left = false;
+
+        enemyHealth = 10;
+
+        enemyBody = this.GetComponent<Rigidbody2D>();
+
+        myTrans = this.transform;
+    }
+
+    // Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (left == false)
+        {
+            //Move right
+            enemySpeed = 5;
+            Vector2 enemyVel = enemyBody.velocity;
+            enemyVel.x = enemySpeed;
+            enemyBody.velocity = enemyVel;
+        }
+        else if (left == true)
+        {
+            //Move left
+            enemySpeed = -5;
+            Vector2 enemyVel = enemyBody.velocity;
+            enemyVel.x = enemySpeed;
+            enemyBody.velocity = enemyVel;
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (left == false)
+        {
+            left = true;
+        }
+        else if (left == true)
+        {
+            left = false;
+        }
+        /*            else if (left = true)
+                    {
+                        left = false;
+                    }
+                }
+        */
+    }
+}
