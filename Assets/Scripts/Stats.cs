88889d61301a2284c215dd7d062e1064ab8884e5
BugiNinja <lassi.tormikoski@gmail.com>
2017-09-23 11:16:47 +0300

    -Changes from Update branch

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour {
+
+    public int initialHp = 100; //Starting HitPoints
+
+    public int maxHp = 100; //Max amount of HitPoints
+
+    public float knockback = 200; //Knockback force when hit
+
+    public float invisibleTime = 1; //Invisible time when hit
+
+    bool alive = true;
+
+    bool invisible = false;
+
+    float invisibleTimer;
+
+    public int hp;
+
+    void Start()
+    {
+        hp = initialHp;
+    }
+
+    void Update()
+    {
+        if (invisible)
+        {
+            InvicibilityTimer();
+        }
+    }
+
+
+    public void AddLife(int amount) 
+    {
+        hp += amount;
+        CheckLife();
+    }
+    public void LoseLife(int amount)
+    {
+        hp -= amount;
+        CheckLife();
+    }
+    public void ResetLife()
+    {
+        hp = initialHp;
+    }
+    public int GetALife()
+    {
+        return hp;
+    }
+    private void CheckLife()
+    {
+        if (hp > maxHp)
+        {
+            hp = maxHp;
+        }
+        else if (hp <= 0)
+        {
+            alive = false;
+        }
+    }
+
+
+    public float GetKnockback()
+    {
+        return knockback;
+    }
+    public void GiveInvicibility()
+    {
+        invisible = true;
+        invisibleTimer = invisibleTime;
+    }
+    public bool IsInvicible()
+    {
+        return invisible;
+    }
+    private void InvicibilityTimer()
+    {
+        if (invisibleTimer > 0)
+        {
+            invisibleTimer -= Time.deltaTime;
+            
+        }
+        else if(invisibleTimer <= 0)
+        {
+            invisible = false;
+        }
+    }
+    
+
+    public bool IsAlive()
+    {
+        return alive;
+    }
+
+
+}
---
+++ Assets/Scripts/gunMouseFollow.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunMouseFollow : MonoBehaviour {
+
+    Vector3 mousePosition;
+
+    public Transform crosshair;
+
+    public GameObject bulletPrefab;
+
+    public GameObject grenadePrefab;
+
+    float lookingSpeed;
+
+    Transform muzzle;
+
+    public float shootingDelay = 1;
+
+    float shootingTimer = 0;
+
+    public float grenadeDelay = 1;
+
+    float grenadeTimer = 0;
+
+    SpriteRenderer sr;
+
+    void Start() {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        muzzle = transform.GetChild(0);
+    }
+
+
+	void Update () {
+        MoveCrosshair();
+        RotateGun();
+        Shoot();
+    }
+    void MoveCrosshair() {
+        mousePosition = Input.mousePosition;
+        mousePosition.z = 1.0f;
+        crosshair.position = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+    void RotateGun() {
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+        if(transform.rotation.z > 0)
+        {
+            sr.flipX = true;
+        }
+        else if (transform.rotation.z < 0)
+        {
+            sr.flipX = false;
+        }
+    }
+    void Shoot() {
+        if (shootingTimer > 0)
+        {
+            shootingTimer -= Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Fire1") && shootingTimer <= 0)
+        {
+            Instantiate(bulletPrefab, muzzle.transform.position, transform.rotation);
+            shootingTimer = shootingDelay;
+            
+        }
+        if (grenadeTimer > 0)
+        {
+            grenadeTimer -= Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Fire2") && grenadeTimer <= 0)
+        {
+            Instantiate(grenadePrefab, muzzle.transform.position , transform.rotation);
+            grenadeTimer = grenadeDelay;
+
+        }
+
+    }
+}
