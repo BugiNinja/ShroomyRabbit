d89c562f5dd504bd19a5d4bc5603f8c1c52ed743
YouMyller <juumyllyla@gmail.com>
2017-09-23 17:54:21 +0300

    Missing things

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+
+    public float enemyNumber;
+    public GameObject nextEnemies;
+    int[] doors = new int[2];
+    int i = 0;
+    // Use this for initialization
+    void Start ()
+    {
+        doors[0] = 2;
+        doors[1] = 2;
+
+       enemyNumber = doors[i];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (enemyNumber <= 0)
+        {
+         //   nextEnemies.SetActive(true);
+            Destroy(transform.GetChild(0).gameObject);
+            i++;
+            enemyNumber = doors[i];
+        }
+	}
+}
---
+++ Assets/Scripts/bigShroom.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bigShroom : MonoBehaviour {
+
+    Stats s;
+
+    // Use this for initialization
+    void Start ()
+    {
+        GetComponent<SpriteRenderer>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Stats s = coll.gameObject.GetComponent<Stats>();  //coll.gameObject.GetComponent<Stats>();
+            s.AddLife(100);
+            Destroy(gameObject);          
+        }
+        else if (coll.gameObject.tag == "Enemy")
+        {
+            Stats s = coll.gameObject.GetComponent<Stats>();  //coll.gameObject.GetComponent<Stats>();
+            s.AddLife(100);
+            Destroy(gameObject);
+        }
+    }
+
+}
